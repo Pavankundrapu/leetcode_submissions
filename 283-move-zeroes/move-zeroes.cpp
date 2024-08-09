70f00a4860a5303eaa51c62559922da98dd772ea
Pavan Kumar Kundrapu <115059910+Pavankundrapu@users.noreply.github.com>
2024-08-09 21:12:00 +0530

    Time: 16 ms (53.60%) | Memory: 21.9 MB (43.11%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,10 +1,12 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int x = 0; // x points to the last found non-zero element
-        for (int y = 0; y < nums.size(); y++) {
-            if (nums[y] != 0) {
-                swap(nums[x], nums[y]);
+        int x = 0;
+        for(int y = 0;y<nums.size();y++)
+        {
+            if(nums[y]!=0)
+            {
+                swap(nums[x],nums[y]);
                 x++;
             }
         }
