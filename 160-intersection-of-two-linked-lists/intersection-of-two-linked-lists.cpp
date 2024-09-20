7c0d87bc0ae82aa5caa15a8000ed4c30dc95c5d8
Pavan Kumar Kundrapu <115059910+Pavankundrapu@users.noreply.github.com>
2024-09-20 22:00:03 +0530

    Time: 38 ms (50.71%) | Memory: 17.3 MB (86.77%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
@@ -8,23 +8,57 @@
  */
 class Solution {
 public:
-    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        unordered_map<ListNode*,bool> mpp;
-        ListNode* temp = headA;
-        while(temp)
-        {
-            mpp[temp] = true;
+
+    ListNode* intersect(ListNode *smaller, ListNode *larger, int d) {
+        ListNode* temp = larger;
+        
+        // Move the larger list ahead by 'd' steps
+        while (d) {
+            d--;
             temp = temp->next;
         }
-        temp = headB;
-        while(temp)
-        {
-            if(mpp.find(temp) != mpp.end())
-            {
+        
+        ListNode* temp2 = smaller;
+        
+        // Traverse both lists and look for the intersection point
+        while (temp && temp2) {
+            if (temp == temp2) {
                 return temp;
             }
             temp = temp->next;
+            temp2 = temp2->next;  // Corrected this part
         }
+        
+        // If no intersection is found
         return nullptr;
+    }
+
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode* temp = headA;
+        int n1 = 0;
+        
+        // Calculate the length of the first list
+        while (temp) {
+            n1++;
+            temp = temp->next;
+        }
+        
+        temp = headB;
+        int n2 = 0;
+        
+        // Calculate the length of the second list
+        while (temp) {
+            n2++;
+            temp = temp->next;
+        }
+        
+        // If the first list is longer, move its pointer ahead by the difference in lengths
+        if (n1 > n2) {
+            return intersect(headB, headA, n1 - n2);
+        } 
+        // If the second list is longer, move its pointer ahead
+        else {
+            return intersect(headA, headB, n2 - n1);
+        }
     }
 };
